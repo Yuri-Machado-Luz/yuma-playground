33155de9dc14bc40366bd0bc49a68c47c11dd437
Yuri Machado <yurimachadoluz@gmail.com>
2026-03-12 15:55:19 -0300

    feat: configurar React 19 como framework base
    - Criar entry point em src/main.tsx
    - Setup React 18+ com StrictMode
    - Configurar index.html com app div
    - Adicionar assets públicos

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './main/Home';
+import Css3Tooling from './web/Css3Tooling/Css3Tooling';
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/css3-tooling"
+					element={<Css3Tooling />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
